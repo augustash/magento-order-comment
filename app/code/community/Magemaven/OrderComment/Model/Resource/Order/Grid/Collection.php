5c08036660b04a67ba08c9bbbb8cfbabc4bd9867
Pete McWilliams <petemcw@gmail.com>
2015-11-18 14:23:36 -0600

    Adds default grid columns to field map to eliminate ambiguous column error

--- app/code/community/Magemaven/OrderComment/Model/Resource/Order/Grid/Collection.php
+++ app/code/community/Magemaven/OrderComment/Model/Resource/Order/Grid/Collection.php
@@ -10,8 +10,34 @@
  * @copyright   Copyright (c) 2011-2012 Sergey Storchay <r8@r8.com.ua>
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
-class Magemaven_OrderComment_Model_Resource_Order_Grid_Collection extends Mage_Sales_Model_Mysql4_Order_Grid_Collection
+class Magemaven_OrderComment_Model_Resource_Order_Grid_Collection extends Mage_Sales_Model_Resource_Order_Grid_Collection
 {
+    /**
+     * Init collection select
+     *
+     * @return  Mage_Core_Model_Resource_Db_Collection_Abstract
+     */
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+
+        // map columns to include table alias
+        $this->addFilterToMap('increment_id', 'main_table.increment_id');
+        $this->addFilterToMap('store_id', 'main_table.store_id');
+        $this->addFilterToMap('billing_name', 'main_table.billing_name');
+        $this->addFilterToMap('shipping_name', 'main_table.shipping_name');
+        $this->addFilterToMap('base_grand_total', 'main_table.base_grand_total');
+        $this->addFilterToMap('grand_total', 'main_table.grand_total');
+        $this->addFilterToMap('status', 'main_table.status');
+
+        return $this;
+    }
+
+    /**
+     * Modify collection after load
+     *
+     * @return  Mage_Core_Model_Resource_Db_Collection_Abstract
+     */
     protected function _afterLoad()
     {
         parent::_afterLoad();
